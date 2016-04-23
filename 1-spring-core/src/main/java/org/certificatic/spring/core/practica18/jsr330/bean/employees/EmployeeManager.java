7b20c359249454c2a58aaa2838678f667bc916c2
Ivan García <ivan.garcia@adesis.com>
2016-04-23 15:04:53 -0500

    laboratorios 18

--- 1-spring-core/src/main/java/org/certificatic/spring/core/practica18/jsr330/bean/employees/EmployeeManager.java
+++ 1-spring-core/src/main/java/org/certificatic/spring/core/practica18/jsr330/bean/employees/EmployeeManager.java
@@ -1,7 +1,12 @@
 package org.certificatic.spring.core.practica18.jsr330.bean.employees;
 
-import org.certificatic.spring.core.practica18.jsr330.bean.Employee;
+import javax.inject.Named;
 
+import org.certificatic.spring.core.practica18.jsr330.bean.Employee;
+import org.certificatic.spring.core.practica18.jsr330.qualifiers.ManagerEmployeeQualifier;
+
+@Named
+@ManagerEmployeeQualifier
 public class EmployeeManager extends Employee {
 
 	public EmployeeManager() {
--- 1-spring-core/src/main/java/org/certificatic/spring/core/practica18/jsr330/bean/employees/EmployeeSecretaryAssistant.java
+++ 1-spring-core/src/main/java/org/certificatic/spring/core/practica18/jsr330/bean/employees/EmployeeSecretaryAssistant.java
@@ -1,7 +1,12 @@
 package org.certificatic.spring.core.practica18.jsr330.bean.employees;
 
+import javax.inject.Named;
+import javax.inject.Singleton;
+
 import org.certificatic.spring.core.practica18.jsr330.bean.Employee;
 
+@Named("secretaryAssistantEmployee")
+@Singleton
 public class EmployeeSecretaryAssistant extends Employee {
 
 	public EmployeeSecretaryAssistant() {
